4bb84af0e4cdc3c3ea5f971d7417a4063fb1206d
Till Spindler <14042276+spindev@users.noreply.github.com>
2024-12-05 15:20:12 +0100

    feat: add ubuntu-dind (docker in docker)

---
+++ ubuntu-dind/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ORG=$ORG
+REG_TOKEN=$REG_TOKEN
+LABELS=$LABELS
+RUNNERGROUP=$RUNNERGROUP
+
+cd /actions-runner || exit
+
+export RUNNER_ALLOW_RUNASROOT="1"
+
+# Start docker
+start-docker.sh
+
+DATA_DIR="/actions-runner/data/$HOSTNAME"
+
+if [ -d "$DATA_DIR" ]; then
+    echo "Runner already exists, copying configuration files..."
+    cp "$DATA_DIR/.credentials" "$DATA_DIR/.runner" "$DATA_DIR/.credentials_rsaparams" /actions-runner/
+else
+    echo "Creating runner data directory..."
+    mkdir -p "$DATA_DIR"
+
+    echo "Runner does not exist, configuring..."
+    ./config.sh --url https://github.com/${ORG} --token ${REG_TOKEN} --name ${HOSTNAME} --runnergroup $RUNNERGROUP --labels $LABELS
+
+    echo "Backing up the configuration files..."
+    cp /actions-runner/.credentials /actions-runner/.credentials_rsaparams /actions-runner/.runner "$DATA_DIR"
+fi
+
+echo "Starting the runner..."
+./run.sh & wait $!
